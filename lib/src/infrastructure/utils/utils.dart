bc2da464ee825ca34755ca1b0ee93b69e9b10936
shayanzare-dev <shayanzare2222@gmail.com>
2025-06-02 18:24:01 +0330

    wip: implement the basic things of home page

--- lib/src/infrastructure/utils/utils.dart
+++ lib/src/infrastructure/utils/utils.dart
@@ -3,11 +3,22 @@ import 'package:flutter/widgets.dart';
 class Utils {
   Utils._();
 
-  static const double _tinySpace = 2;
-  static const double _smallSpace = 4;
+  static const double tinySpace = 2;
+  static const double smallSpace = 4;
+  static const double mediumSpace = 8;
+  static const double semiLargeSpace = 12;
+  static const double largeSpace = 16;
+  static const double giantSpace = 32;
 
-  static const SizedBox tinyVerticalSpace = SizedBox(height: _tinySpace);
-
-  static const tinyPadding = EdgeInsets.all(_tinySpace);
-  static const smallPadding = EdgeInsets.all(_smallSpace);
+  // ---------------
+  static const SizedBox tinyVerticalSpace = SizedBox(height: tinySpace);
+  static const SizedBox smallVerticalSpace = SizedBox(height: smallSpace);
+  static const SizedBox mediumVerticalSpace = SizedBox(height: mediumSpace);
+  static const SizedBox semiLargeVerticalSpace = SizedBox(height: semiLargeSpace);
+  // ---------------
+  static const SizedBox smallHorizontalSpace = SizedBox(height: smallSpace);
+  // ---------------
+  static const tinyPadding = EdgeInsets.all(tinySpace);
+  static const smallPadding = EdgeInsets.all(smallSpace);
+  static const mediumPadding = EdgeInsets.all(mediumSpace);
 }
