e8e16200c7f7e730454e1077833c2959724266f8
shayanzare-dev <shayanzare2222@gmail.com>
2025-06-11 22:12:54 +0330

    chore: add theme_names.dart

---
+++ lib/src/infrastructure/theme/theme_names.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class ThemeNames {
+  ThemeNames._();
+
+  static const Color primaryColor = Color(0xff5D4037);
+  static const Color disableColor = Color(0xFFCACACA);
+  static const Color headingColor = Color(0xFF3C3C3C);
+  static const Color paragraphColor = Color(0xFF555555);
+  static const Color goldenColor = Color(0xFFFFB800);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
--- lib/src/infrastructure/theme/themes.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class Themes {
-  Themes._();
-
-  static const Color primaryColor = Color(0xff5D4037);
-  static const Color secondaryColor = Color(0xffCACACA);
-}
