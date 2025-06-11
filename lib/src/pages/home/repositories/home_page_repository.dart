1a169eb0db08f7ce734df1b7ca5d1ddb43c5900d
shayanzare-dev <shayanzare2222@gmail.com>
2025-06-11 21:10:13 +0330

    feat: fetch menu items from API, handle errors with Either

--- lib/src/pages/home/repositories/home_page_repository.dart
+++ lib/src/pages/home/repositories/home_page_repository.dart
@@ -4,6 +4,7 @@ import 'package:either_dart/either.dart';
 import 'package:http/http.dart' as http;
 
 import '../../../infrastructure/commons/repository_urls.dart';
+import '../models/menu_item_view_model.dart';
 import '../models/offer_banner_view_model.dart';
 
 class HomePageRepository {
@@ -22,6 +23,28 @@ class HomePageRepository {
         return Right(banners);
       } else {
         return const Left('failed to get offer banners');
+      }
+    } catch (e) {
+      return Left(e.toString());
+    }
+  }
+
+  Future<Either<String, List<MenuItemViewModel>>> getMenuItems(
+    int productType,
+  ) async {
+    try {
+      int? statusCode;
+      final http.Response response = await http.get(
+        RepositoryUrls.getMenuItems(productType),
+      );
+      final List<dynamic> jsonData = json.decode(response.body);
+      statusCode = response.statusCode;
+      if (statusCode == 200) {
+        final List<MenuItemViewModel> items =
+            jsonData.map((json) => MenuItemViewModel.fromJson(json)).toList();
+        return Right(items);
+      } else {
+        return const Left('failed to get menu items');
       }
     } catch (e) {
       return Left(e.toString());
