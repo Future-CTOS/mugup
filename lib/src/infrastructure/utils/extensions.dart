e1a9eb27eb9c16f62ce0489f45f19ece618cc3fe
shayanzare-dev <shayanzare2222@gmail.com>
2025-06-19 18:55:09 +0330

    chore: add get size of screen variables from current context

---
+++ lib/src/infrastructure/utils/extensions.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+extension MugUpExentension on BuildContext {
+  Size get screenSize => MediaQuery.of(this).size;
+
+  double get screenWidth => screenSize.width;
+
+  double get screenHeight => screenSize.height;
+}
