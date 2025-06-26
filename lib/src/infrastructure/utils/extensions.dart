df70d2d9276c799014b1d80979c9e1e876c04674
shayanzare-dev <shayanzare2222@gmail.com>
2025-06-26 09:37:30 +0330

    refactor: improve BuildContext extension naming and imports

--- lib/src/infrastructure/utils/extensions.dart
+++ lib/src/infrastructure/utils/extensions.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
-extension MugUpExentension on BuildContext {
+extension ContextExtension on BuildContext {
   Size get screenSize => MediaQuery.of(this).size;
 
   double get screenWidth => screenSize.width;
