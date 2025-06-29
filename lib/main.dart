c500e97b5771ecabec06c8c580b83609c37b4163
shayanzare-dev <shayanzare2222@gmail.com>
2025-06-29 18:53:13 +0330

    chore: change location of main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-import 'app.dart';
+import 'src/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
